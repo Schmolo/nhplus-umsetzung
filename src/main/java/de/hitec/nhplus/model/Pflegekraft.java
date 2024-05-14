60bb1b3e4e2cf97218391b503f9ca662d12af7ca
Adrian Berger <adrian.berger@zarm.uni-bremen.de>
2024-05-14 13:07:50 +0200

    Add Pflegekraft model and Password utils

--- src/main/java/de/hitec/nhplus/model/Pflegekraft.java
+++ src/main/java/de/hitec/nhplus/model/Pflegekraft.java
@@ -1,2 +1,86 @@
-package de.hitec.nhplus.model;public class Pflegekraft {
+package de.hitec.nhplus.model;
+
+import de.hitec.nhplus.utils.DateConverter;
+import de.hitec.nhplus.utils.PasswordUtil;
+import javafx.beans.property.SimpleLongProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+import java.time.LocalDate;
+
+public class Pflegekraft extends Person {
+    private SimpleLongProperty pid;
+    private final SimpleStringProperty dateOfBirth;
+    private final SimpleStringProperty password_hash;
+
+    /**
+     * This is a constructor for the Pflegekraft class.
+     *
+     * @param firstName The first name of the Pflegekraft.
+     * @param surname The surname of the Pflegekraft.
+     * @param dateOfBirth The date of birth of the Pflegekraft. It is a LocalDate object.
+     * @param password The password for the Pflegekraft. This is not the actual password, but a string that will be hashed to create the password.
+     */
+    public Pflegekraft(String firstName, String surname, LocalDate dateOfBirth, String password) {
+        super(firstName, surname);
+        this.dateOfBirth = new SimpleStringProperty(DateConverter.convertLocalDateToString(dateOfBirth));
+        this.password_hash = new SimpleStringProperty(PasswordUtil.generatePassword(password));
+    }
+
+    /**
+     * This is an overloaded constructor for the Pflegekraft class.
+     *
+     * @param pid The unique identifier for the Pflegekraft.
+     * @param firstName The first name of the Pflegekraft.
+     * @param surname The surname of the Pflegekraft.
+     * @param dateOfBirth The date of birth of the Pflegekraft. It is a LocalDate object.
+     * @param password_hash The password hash for the Pflegekraft.
+     */
+    public Pflegekraft(long pid, String firstName, String surname, LocalDate dateOfBirth, String password_hash) {
+        super(firstName, surname);
+        this.pid = new SimpleLongProperty(pid);
+        this.dateOfBirth = new SimpleStringProperty(DateConverter.convertLocalDateToString(dateOfBirth));
+        this.password_hash = new SimpleStringProperty(password_hash);
+    }
+
+    public long getPid() {
+        return pid.get();
+    }
+
+    public SimpleLongProperty pidProperty() {
+        return pid;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth.get();
+    }
+
+    public SimpleStringProperty dateOfBirthProperty() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth.set(dateOfBirth);
+    }
+
+    public String getPassword_hash() {
+        return password_hash.get();
+    }
+
+    public SimpleStringProperty password_hashProperty() {
+        return password_hash;
+    }
+
+    public void setPassword_hash(String password_hash) {
+        this.password_hash.set(password_hash);
+    }
+
+    @Override
+    public String toString() {
+        return "Pflegekraft\n" +
+                "pid: " + pid.get() + "\n" +
+                "firstName: " + getFirstName() + "\n" +
+                "surname: " + getSurname() + "\n" +
+                "dateOfBirth: " + dateOfBirth.get() + "\n" +
+                "\n";
+    }
 }
--- src/main/java/de/hitec/nhplus/utils/PasswordUtil.java
+++ src/main/java/de/hitec/nhplus/utils/PasswordUtil.java
@@ -1,2 +1,50 @@
-package de.hitec.nhplus.utils;public class PasswordUtil {
+package de.hitec.nhplus.utils;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+public class PasswordUtil {
+    private static final String ALGORITHM = "SHA-512";
+
+    public static String generatePassword(String password) {
+        String salt = generateSalt();
+        String hashed_password = hashPassword(password, salt);
+        return salt + ":" + hashed_password;
+    }
+
+    public static String hashPassword(String password, String salt) {
+        try {
+            MessageDigest md = MessageDigest.getInstance(ALGORITHM);
+            md.update(salt.getBytes());
+            byte[] bytes = md.digest(password.getBytes());
+            return Base64.getEncoder().encodeToString(bytes);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String generateSalt() {
+        SecureRandom random = new SecureRandom();
+        byte[] bytes = new byte[20];
+        random.nextBytes(bytes);
+        return Base64.getEncoder().encodeToString(bytes);
+    }
+
+    public static boolean checkPassword(String password, String hashedPassword) {
+        String[] parts = hashedPassword.split(":");
+        String salt = parts[0];
+        String hashedInputPassword = hashPassword(password, salt);
+        return hashedInputPassword.equals(parts[1]);
+    }
+
+    public static void main(String[] args) {
+        String password = "password";
+        String hashedPassword = generatePassword(password);
+        Boolean checkPassword = checkPassword(password, generatePassword(password));
+        System.out.println(password);
+        System.out.println(hashedPassword);
+        System.out.println(checkPassword);
+    }
 }
