39f6e0324d0b4d571e25a3b96bcd24f8876ce47d
Adrian Berger <75171839+Schmolo@users.noreply.github.com>
2024-06-04 20:22:28 +0200

    Add Docs to CaregiverController

--- src/main/java/de/hitec/nhplus/controller/CaregiverController.java
+++ src/main/java/de/hitec/nhplus/controller/CaregiverController.java
@@ -13,33 +13,54 @@ import de.hitec.nhplus.model.Caregiver;
 import java.sql.SQLException;
 import java.time.LocalDate;
 
+/**
+ * This class is the controller for the Caregiver view.
+ * It handles the interaction between the user and the UI for managing caregivers.
+ */
 public class CaregiverController {
 
     @FXML
+    // TextField for the first name of the caregiver
     private TextField textFieldFirstName;
 
     @FXML
+    // TextField for the surname of the caregiver
     private TextField textFieldSurname;
 
     @FXML
+    // DatePicker for the date of birth of the caregiver
     private DatePicker datePickerDateOfBirth;
 
     @FXML
+    // TextField for the telephone number of the caregiver
     private TextField textFieldTelephoneNumber;
 
     @FXML
+    // TextField for the username of the caregiver
     private TextField textFieldUsername;
 
     @FXML
+    // PasswordField for the password of the caregiver
     private PasswordField passwordFieldPassword;
 
     @FXML
+    // CheckBox for the admin status of the caregiver
     private CheckBox checkBoxIsAdmin;
 
+    // The controller for the AllCaregiver view
     private AllCaregiverController controller;
+    // The caregiver being managed
     private Caregiver caregiver;
+    // The stage of the Caregiver view
     private Stage stage;
 
+    /**
+     * This method initializes the CaregiverController with the given AllCaregiverController, stage, and caregiver.
+     *
+     * @param controller The AllCaregiverController.
+     * @param stage The stage.
+     * @param caregiver The caregiver being managed.
+     */
     public void initialize(AllCaregiverController controller, Stage stage, Caregiver caregiver) {
         this.controller = controller;
         this.stage = stage;
@@ -48,6 +69,9 @@ public class CaregiverController {
         showData();
     }
 
+    /**
+     * This method shows the data of the caregiver in the UI.
+     */
     private void showData(){
         this.textFieldFirstName.setText(caregiver.getFirstName());
         this.textFieldSurname.setText(caregiver.getSurname());
@@ -57,6 +81,10 @@ public class CaregiverController {
         this.checkBoxIsAdmin.setSelected(caregiver.isAdmin());
     }
 
+    /**
+     * This method handles the change of the caregiver data.
+     * It validates the input data and updates the caregiver if the data is valid.
+     */
     @FXML
     public void handleChange(){
         if (!areInputDataInvalid()) {
@@ -83,6 +111,9 @@ public class CaregiverController {
         stage.close();
     }
 
+    /**
+     * This method updates the caregiver in the database.
+     */
     private void doUpdate() {
         CaregiverDao dao = DaoFactory.getDaoFactory().createCaregiverDAO();
         try {
@@ -93,19 +124,36 @@ public class CaregiverController {
         }
     }
 
+    /**
+     * This method handles the cancellation of the caregiver data change.
+     * It closes the stage.
+     */
     @FXML
     public void handleCancel(){
         stage.close();
     }
 
+    /**
+     * This method checks if the input data for the caregiver is invalid.
+     *
+     * @return True if the input data is invalid, false otherwise.
+     */
     private boolean areInputDataInvalid() {
         return this.textFieldFirstName.getText() != null && this.textFieldSurname.getText() != null && this.textFieldTelephoneNumber.getText() != null && this.textFieldUsername.getText() != null;
     }
 
+    /**
+     * This method checks if the telephone number of the caregiver is valid.
+     *
+     * @return True if the telephone number is valid, false otherwise.
+     */
     private boolean isTelephoneValid() {
         return this.textFieldTelephoneNumber.getText().matches("^[0-9\\-\\s]{4,15}$");
     }
 
+    /**
+     * This method shows an error alert if the input data for the caregiver is invalid.
+     */
     private void invalidData() {
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle("Invalid Input");
@@ -114,6 +162,9 @@ public class CaregiverController {
         alert.showAndWait();
     }
 
+    /**
+     * This method shows an error alert if the telephone number of the caregiver is invalid.
+     */
     private void invalidTelephone() {
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle("Invalid Input");
